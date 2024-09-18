f3f8c8bdf6d4f1a0f9063ddb50111119b49cd06e
lthnhuw <nhub2106803@student.ctu.edu.vn>
2024-09-18 09:52:28 +0700

    Cai dat du an

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
-    return res.status(console.error.statusCode || 500).json({
+    return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
     
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const contacts = require("../controllers/contact.controller");
 
-const router = express. Router();
+const router = express.Router();
 
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
     .delete(contacts.deleteAll);
+
 router.route("/favorite")
     .get(contacts.findAllFavorite);
 
